ab6ca812227cc852626af6c34455c009edd27b70
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-19 15:23:01 +0200

    feat: added true linked list types (aka seperated the tree

--- includes/zstd/structs/linkedlist.h
+++ includes/zstd/structs/linkedlist.h
@@ -10,9 +10,14 @@
  * @param ... the inline elements of the structure
  */
 #define LinkedList(typeName, ...) \
-    typedef struct typeName { \
+    typedef struct typeName_tree { \
         __VA_ARGS__ \
-        struct typeName* next; \
+        struct typeName_tree* next; \
+    } typeName_tree; \
+    \
+    typedef struct typeName { \
+        typeName_tree* tree; \
+        typeName_tree* curr; \
     } typeName; 
 
 /**
@@ -21,9 +26,16 @@
  * @param ... the inline elements of the structure
  */
 #define DualLinkedList(typeName, ...) \
-    typedef struct typeName { \
+    typedef struct typeName_tree { \
         __VA_ARGS__ \
-        struct typeName* prev; \
-        struct typeName* next; \
-    } typeName; 
+        struct typeName_tree* prev; \
+        struct typeName_tree* next; \
+    } typeName_tree; \
+    \
+    typedef struct typeName { \
+        typeName_tree* tree; \
+        typeName_tree* next; \
+    }; typeName 
+
         
+
