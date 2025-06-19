f301b9cef93d18e73531db8220ad2df5a7f44069
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-19 15:13:52 +0200

    feat: added quick importing headers

---
+++ includes/zstd.h
@@ -0,0 +1,8 @@
+/**
+ * ZSTD Definitions
+ * This header doesn't contain any code and is merely here for quick including multiple elements
+ */
+
+#pragma once
+
+#include <zstd/stdstructs.h>
---
+++ includes/zstd/stdstructs.h
@@ -0,0 +1,8 @@
+/**
+ * ZSTD Structure definitions. 
+ * This header doesn't contain any code and is merely here for quick including multiple structures
+ */
+
+#pragma once
+
+#include <zstd/structs/linkedlist.h>
