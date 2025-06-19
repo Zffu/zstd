149ac294450055d4acdea959ec0968feb1991461
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-19 15:11:11 +0200

    feat: added LinkedList helper

---
+++ includes/zstd/structures.h
@@ -0,0 +1,16 @@
+/**
+ * ZSTD Structure definitions
+ */
+
+#pragma once
+
+/**
+ * Creates a linked list structure (an ordered set of data elements, each containing a link to its successor).
+ * @param typeName the name of the structure / type
+ * @param ... the inline elements of the structure
+ */
+#define LinkedList(typeName, ...) \
+    typedef struct typeName { \
+        __VA_ARGS__ \
+        struct typeName* next; \
+    }; \
