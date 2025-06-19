9116ec3e8225a4f58ffe8f72b271d9e4b6bcf7c2
Zffu <103074097+Zffu@users.noreply.github.com>
2025-06-19 15:19:09 +0200

    feat: added dual linked list

--- includes/zstd/structs/linkedlist.h
+++ includes/zstd/structs/linkedlist.h
@@ -13,4 +13,17 @@
     typedef struct typeName { \
         __VA_ARGS__ \
         struct typeName* next; \
-    } typeName; \
+    } typeName; 
+
+/**
+ * Creates a dual linked list structure with both a next and previous indicator (an ordered set of data elements, each containing a link to its successor and its predecessor).
+ * @param typeName the name of the structure / type
+ * @param ... the inline elements of the structure
+ */
+#define DualLinkedList(typeName, ...) \
+    typedef struct typeName { \
+        __VA_ARGS__ \
+        struct typeName* prev; \
+        struct typeName* next; \
+    } typeName; 
+        
